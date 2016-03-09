1d1177ad403823db88a0b948bcaca8b88cacf519
Max Goldstein <maxgoldstein1@gmail.com>
2016-03-09 10:10:22 -0500

    Change ga variables to ra.
    This will hopefully reinforce the idea that they are "readers" that return a.

--- src/Function/Extra.elm
+++ src/Function/Extra.elm
@@ -35,73 +35,73 @@ map =
 
 {-| Send a single argument `x` into a binary function using two intermediate mappings.
 
-    (map2 f ga gb) x == (f (ga x) (gb x)) x
+    (map2 f ra rb) x == (f (ra x) (rb x)) x
 
 This allows `map2` to read two variables from the environment `x` before applying them to a binary function `f`.
 -}
 map2 : (a -> b -> c) -> (x -> a) -> (x -> b) -> x -> c
-map2 f ga gb x =
-  f (ga x) (gb x)
+map2 f ra rb x =
+  f (ra x) (rb x)
 
 
 {-| Send a single argument `x` into a ternary function using three intermediate mappings.
 
-    (map3 f ga gb gc) x == (f (ga x) (gb x) (gc x)) x
+    (map3 f ra rb rc) x == (f (ra x) (rb x) (rc x)) x
 
 This allows `map3` to read three variables from the environment `x` before applying them to a ternary function `f`.
 -}
 map3 : (a -> b -> c -> d) -> (x -> a) -> (x -> b) -> (x -> c) -> x -> d
-map3 f ga gb gc x =
-  f (ga x) (gb x) (gc x)
+map3 f ra rb rc x =
+  f (ra x) (rb x) (rc x)
 
 
 {-| Send a single argument `x` into a quaternary function using four intermediate mappings.
 Use `andMap` as an infix combinator in order to deal with a larger numbers of arguments.
 
-    (map4 f ga gb gc gd) x == (f (ga x) (gb x) (gc x) (gd x)) x
+    (map4 f ra rb rc gd) x == (f (ra x) (rb x) (rc x) (gd x)) x
 
 This allows `map4` to read four variables from the environment `x` before applying them to a quaternary function `f`.
 -}
 map4 : (a -> b -> c -> d -> e) -> (x -> a) -> (x -> b) -> (x -> c) -> (x -> d) -> x -> e
-map4 f ga gb gc gd x =
-  f (ga x) (gb x) (gc x) (gd x)
+map4 f ra rb rc gd x =
+  f (ra x) (rb x) (rc x) (gd x)
 
 
 {-| Incrementally apply more functions, similar to `map`*N* where *N* is not fixed. This allows `andMap` to read an arbitrary number of arguments from the same environment `x`.
 
 These are all equivalent:
 
-    (f `andMap` ga `andMap` gb `andMap` gc) x
-    f x (ga x) (gb x) (gc x)
-    (map4 identity f ga gb gc) x
-    (identity `map` f `andMap` ga `andMap` gb `andMap` gc) x
+    (f `andMap` ra `andMap` rb `andMap` rc) x
+    f x (ra x) (rb x) (rc x)
+    (map4 identity f ra rb rc) x
+    (identity `map` f `andMap` ra `andMap` rb `andMap` rc) x
 
 As are all of these:
 
-    (f' `map` ga `andMap` gb `andMap` gc) x
-    f' (ga x) (gb x) (gc x) x
-    (map3 f' ga gb gc) x
+    (f' `map` ra `andMap` rb `andMap` rc) x
+    f' (ra x) (rb x) (rc x) x
+    (map3 f' ra rb rc) x
 
 Also notice the type signatures...
 
-    ga                                      : x -> a
-    gb                                      : x -> b
-    gc                                      : x -> c
+    ra                                      : x -> a
+    rb                                      : x -> b
+    rc                                      : x -> c
 
     f                                       : x -> a -> b -> c -> d
-    (f `andMap` ga)                         : x -> b -> c -> d
-    (f `andMap` ga `andMap` gb)             : x -> c -> d
-    (f `andMap` ga `andMap` gb `andMap` gc) : x -> d
+    (f `andMap` ra)                         : x -> b -> c -> d
+    (f `andMap` ra `andMap` rb)             : x -> c -> d
+    (f `andMap` ra `andMap` rb `andMap` rc) : x -> d
 
     f'                                      : a -> b -> c -> d
-    (f' `map` ga)                           : x -> b -> c -> d
-    (f' `map` ga `andMap` gb)               : x -> c -> d
-    (f' `map` ga `andMap` gb `andMap` gc)   : x -> d
+    (f' `map` ra)                           : x -> b -> c -> d
+    (f' `map` ra `andMap` rb)               : x -> c -> d
+    (f' `map` ra `andMap` rb `andMap` rc)   : x -> d
 
 -}
 andMap : (x -> a -> b) -> (x -> a) -> x -> b
-andMap f ga x =
-  f x (ga x)
+andMap f ra x =
+  f x (ra x)
 
 
 {-| Connect the result `a` of the first function to the first argument of the second function to form a pipeline.
